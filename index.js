7e704144f933b53664591d671a354170683b52c8
Preston Elliott <preston.elliott24@gmail.com>
2019-01-26 13:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
+
+function destructivelyAppendKitten(kittens, element) {
+  kittens.push(element);
+  return kittens;
+}
+
+
+
 
 // Add your functions and code here
