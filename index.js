faa5bf1c0020210cfe475db3ba3a0f30c61b9d21
Preston Elliott <preston.elliott24@gmail.com>
2019-01-26 13:54:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten(array, element) {
 }
 
 function appendKitten(array, element) {
-  [...kittens, "Broom"];
+  return [...kittens, "Broom"];
 }
 
 // Add your functions and code here
