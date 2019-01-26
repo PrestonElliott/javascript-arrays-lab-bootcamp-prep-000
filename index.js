dfb15f327c0f92cce653280af95b06d13b73e851
Preston Elliott <preston.elliott24@gmail.com>
2019-01-26 13:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
-function destructivelyAppendKitten() {
+function destructivelyAppendKitten(array) {
 array.push("Ralph");
 return array;
 }
